9bd8f022c4267e95b6174f8dc565154dde2289f1
picnii <sompop@picnii.me>
2014-03-10 11:23:05 +0700

    BONUS

--- js/Controller.js
+++ js/Controller.js
@@ -2,15 +2,15 @@ function MainCtrl($scope)
 {
 	$scope.sum = 0;
 	$scope.products = [
-		{name:"ส้มตำ", price:20},
-		{name:"แกงส้ม", price:30},
-		{name:"น้ำตกหมู", price:40},
-		{name:"ต้ำยำ", price:50}
+		{name:"ส้มตำ", price:20, quantity:2},
+		{name:"แกงส้ม", price:30, quantity:3},
+		{name:"น้ำตกหมู", price:40, quantity:1},
+		{name:"ต้ำยำ", price:50, quantity:1}
 
 	]
 
 	for(var i =0; i < $scope.products.length ;i++)
-		$scope.sum += $scope.products[i].price;
+		$scope.sum += $scope.products[i].price * $scope.products[i].quantity;
 
 	//tinyurl.com/tsrdoc
 }
